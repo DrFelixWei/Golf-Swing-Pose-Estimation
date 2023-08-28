de29b97458c96123c784359da256c29e158f2ca8
Felix Wei <felixwei7@gmail.com>
2023-08-28 16:24:06 -0700

    setup blueprint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+
+import '@tensorflow/tfjs-backend-webgl';
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from '@tensorflow/tfjs-core';
+
 
 function App() {
+
+  // Handle user uploading video source
+
+
+  // Variable to hold current frame image
+
+
+  // Add timeupdate event listeners to video to update currentFrame variable
+
+
+  // Variable to hold current frame with pose estimation drawn on
+
+
+
+  // Variable to hold if currently pose estimation in progress already
+
+
+  // POSE ESTIMATION
+    // Set pose estimation in progress variable to true
+
+
+
+    // Draw and update variable to hold current frame with pose estimation drawn on
+
+
+
+    // Set pose estimation in progress variable to false
+
+
+
+  // Add event listener / trigger for whenever currentFrameWithPose is updated and is not null to display it on canvas
+
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {/* Button for user to upload video */}
+
+      {/* Display video controls with no visible video */}
+
+
+
+
+
     </div>
   );
 }
