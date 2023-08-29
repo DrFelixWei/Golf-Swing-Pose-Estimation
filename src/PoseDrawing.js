d1ec23a628cf791e72068acb545fbef93743e721
Felix Wei <felixwei7@gmail.com>
2023-08-29 15:59:52 -0700

    keypoints drawing

---
+++ src/PoseDrawing.js
@@ -0,0 +1,53 @@
+export function drawKeypoint(context, name, x, y, z) {
+
+   // not using z for now because not rendering in 3d space, will use in angle measurement later
+
+   // Filter out undesired keypoints
+   const targetKeypoints = ["left_eye", "right_eye", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow",
+                             "left_wrist", "right_wrist", "left_index", "right_index", "left_hip", "right_hip",
+                             "left_knee", "right_knee", "left_heel", "right_heel", "left_ear", "right_ear",
+                             "left_foot_index", "right_foot_index"];
+   if (targetKeypoints.includes(name)) {
+
+     // Draw keypoint white
+     context.fillStyle = 'white';
+     // Draw keypoint location and size
+     context.fillRect(x, y, 10, 10);
+   }
+   console.log(name + " drawn.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export function drawLine(context, keypoint1, keypoint2) {
+
+  // console.log(keypoint1);
+  // console.log(keypoint2);
+
+  var startX = keypoint1.x;
+  var startY = keypoint1.y;
+  var endX = keypoint2.x;
+  var endY = keypoint2.y;
+
+  context.beginPath();
+  context.moveTo(startX, startY);
+  context.lineTo(endX, endY);
+  context.strokeStyle = 'white'; 
+  context.lineWidth = 2;
+  context.stroke();
+}
+
+// Angles to measure
+  // ears to eyes to measure angle of head
+  // hip angle
+  // head drop
+  // shoulder rotation
--- src/PoseEstimation.js
+++ src/PoseEstimation.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
-
 import '@tensorflow/tfjs-backend-webgl';
 import * as poseDetection from '@tensorflow-models/pose-detection';
 import * as tf from '@tensorflow/tfjs-core';
+
+import {drawKeypoint, drawLine} from './PoseDrawing';
+
 
 // Load BlazePoze
 const model = poseDetection.SupportedModels.BlazePose;
@@ -40,21 +41,87 @@ export async function readyTf(videoHeight, videoWidth) {
 // Draw pose image
 export async function drawPose(detector, currentFrame, width, height) {
 
-  // POSE ESTIMATION
+  // Create pose with model from current frame
   const pose = await detector.estimatePoses(currentFrame);
   console.log("pose", pose)
 
-
   // Create and setup canvas to draw pose on
   const canvas = document.createElement('canvas')
+  canvas.width = width;
+  canvas.height = height;
+  const context = canvas.getContext('2d');
+
+  // Iterate through each keypoint and draw it onto canvas
+  pose.forEach((item) => {
+    item.keypoints.forEach((keypoint) => {
+      drawKeypoint(context, keypoint.name, keypoint.x, keypoint.y, keypoint.z)
+    });
+  }); 
+
+  // Connect keypoints to draw skeleton pose
+    if (pose.length !== 0) {
+      // hips
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_hip"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_hip")
+        )
+      // shoulders
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_shoulder"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_shoulder")
+        )
+      // upper arms
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_shoulder"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_elbow")
+        )
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_shoulder"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_elbow")
+        )
+      // lower arms
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_elbow"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_wrist")
+        )
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_elbow"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_wrist")
+        )
+      // hands
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_wrist"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "left_index")
+        )
+      drawLine(context, 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_wrist"), 
+        pose[0].keypoints.find(keypoint => keypoint.name === "right_index")
+        )        
+      // neck spine
+      // back spine
+      // upper legs
+      // lower legs
+
+        
+      // console.log("context", context)
+    }
 
 
 
 
-  // Image to return to app.js as 
+  // Create an iamge from the canvas to return to app.js as poseFrame variable
   var poseImage = new Image();
   return poseImage;
 }
-
   
 
+
+
+
+
+
+// Angles to measure
+  // ears to eyes to measure angle of head
+  // hip angle
+  // head drop
+  // shoulder rotation
